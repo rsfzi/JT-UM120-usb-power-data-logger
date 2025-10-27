63b61d398054b52d4b67d8f4c272b3520389ab7c
Ralf Sieger <sieger@fzi.de>
2025-10-27 12:23:44 +0100

    updated API

---
+++ usb_meter/__init__.py
@@ -0,0 +1,5 @@
+from .measurement import ElectricalMeasurement
+from .stop_provider import StopProvider
+from .data_logger import DataLogger
+from .device import get_devices, devices_by_vid_pid, devices_by_serial_number
+from .usb_meter import USBMeter
