fa522da355388d099b07473f686401172cf2642f
Ralf Sieger <sieger@fzi.de>
2025-11-03 10:40:41 +0100

    added package index

--- usb_meter/__init__.py
+++ usb_meter/__init__.py
@@ -3,3 +3,8 @@ from .stop_provider import StopProvider
 from .data_logger import DataLogger
 from .device import all_devices, devices_by_vid_pid, devices_by_serial_number
 from .usb_meter import USBMeter
+
+__all__ = [
+        "ElectricalMeasurement", "StopProvider", "DataLogger", "USBMeter",
+        "all_devices", "devices_by_vid_pid", "devices_by_serial_number",
+        ]
